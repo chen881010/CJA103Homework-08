f071d08d127ef82f5d49158d439f55b45a248fa6
Sam Chen <cyc881010@icloud.com>
2025-10-20 16:05:33 +0800

    first commit

---
+++ src/hw1/Homework1.java
@@ -0,0 +1,17 @@
+package hw1;
+
+public class Homework1 {
+
+	public static void main(String[] args) {
+	
+	System.out.println("mastermaster");
+	System.out.println("testtest");
+	System.out.println("seconds line");
+	System.out.println("the third line on test branch");
+	System.out.println("the 4th line");
+	System.out.println("the 5th line");
+	System.out.println("the 6th line");
+
+	}
+	
+}
